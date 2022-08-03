5315083afc5da9db4cc0c42d8010e4a8193034fc
ghayas9 <ghayasudin999@gmail.com>
2022-08-03 14:15:59 +0500

    login

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -2,9 +2,39 @@ import React from 'react';
 import '../Login/Login.css';
 import { useNavigate } from "react-router-dom";
 
+//***************API*****************//
+import axios from '../../config/axios';
+import { useState } from 'react';
+//***************API*****************//
+
 const Login = () => {
 
-    const navigate = useNavigate()
+//***********API***************//
+const navigate = useNavigate()
+
+const [email,setEmail]=useState('')
+const [password,setPassword]=useState('')
+
+const LogIn = async()=>{
+    console.log(email,password);
+    try{
+        const res = await axios({
+            method:'post',
+            url:'/company/login',
+            data:{email,password}
+          })
+          console.log(res.data)
+          alert('login successfully')
+          navigate('/companydetails')
+        //   navigate('/registercompanies')
+    }catch(err){
+        alert(err.response.data.message)
+    }
+}
+
+//***********API***************//
+
+    
     
   return (
     <div className='loginMain'>
@@ -17,16 +47,25 @@ const Login = () => {
                     <label className='emailLabel'>
                         Email
                     </label>
-                    <input type='text' className='input_css' />
+                    {/* ADD USE STATE */}
+                    <input type='text' className='input_css' value={email} 
+                    onChange={(e)=>setEmail(e.target.value)}
+                    />
+                    {/* ADD USE STATE */}
                     <label className='passwordLabel'>
                         Password
                     </label>
-                    <input type='password' className='input_css' />
+
+                    {/* ADD USE STATE */}
+                    <input type='password' className='input_css' value={password} 
+                    onChange={(e)=>setPassword(e.target.value)}
+                    />
+                    {/* ADD USE STATE */}
                 </form>
                 <div className='forgotPassword'>
                     Forget Password?
                 </div>
-                <div className='Login_sumit_butn' onClick={() => navigate('/registercompanies')}>
+                <div className='Login_sumit_butn' onClick={LogIn}>
                     Login
                 </div>
 
